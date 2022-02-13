b9173d0d369b195aa8a59e114042e2342d159305
Landon Hughes <landonhughesdeveloper@gmail.com>
2022-02-13 17:57:14 -0500

    first commit

--- ToDoey/ViewController.swift
+++ ToDoey/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "ToDoey"
+    }
+    
 }
 
